fb1c51dbe9e0970e389a6c58e7b5479fadb00559
ecaracasdev <ecaracasdev@gmail.com>
2021-08-18 15:10:40 -0300

    add mongo connections

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-	port: process.env.PORT || 3000
+	port: process.env.PORT || 3000,
+	db_password: process.env.DB_PASSWORD || '8CA76Oy4QUjBJWwc',
+	db_name: process.env.DB_NAME || 'notes'
 }
---
+++ mongo.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const { db_password, db_name } = require('./config')
+const connectionString = `mongodb+srv://db_user_elias:${db_password}@eliascluster.qrdbb.mongodb.net/${db_name}?retryWrites=true&w=majority`
+
+mongoose.connect(connectionString, {
+	useCreateIndex:true,
+	useUnifiedTopology:true,
+	useFindAndModify:false,
+	useNewUrlParser:true
+})
+	.then(() => {
+		console.log('Database connected')
+	})
+	.catch(err => {
+		console.error(err)
+	})
